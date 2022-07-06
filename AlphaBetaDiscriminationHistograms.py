f7b951a9ec835d8f1c29137ef6a6d349c832103e
Skipper (Emma) <lemmaammel2@gmail.com>
2022-07-05 18:19:51 -0700

    error fixes!

--- AlphaBetaDiscriminationHistograms.py
+++ AlphaBetaDiscriminationHistograms.py
@@ -7,6 +7,7 @@ import os, sys, time, math
 import numpy as np
 from array import array
 import matplotlib as m
+import argparse
 m.use('Agg')
 
 #Imports particularly important for our purposes
@@ -15,17 +16,25 @@ import rat
 import csv
 import math
 import matplotlib.pyplot as p
-import significantFigures as s
-import findRatio as f
+#import significantFigures as s
+#import findRatio as f
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--filetype', '-a', type = str, default = '', help = 'File type of alpha and beta files ("ntuple" OR "ratds")')
+parser.add_argument('--filetype', '-f', type = str, default = '', help = 'File type of alpha and beta files ("ntuple" OR "ratds")')
 parser.add_argument('--alphafile', '-a', type = str, default = '', help = 'Alpha file to use (no ".root" extension)')
 parser.add_argument('--betafile', '-b', type = str, default = '', help = 'Beta file to use (no ".root" extension)')
 parser.add_argument('--shape', '-s', type = str, default = 'list', help = 'Shape for plot ("list" OR "square")')
+parser.add_argument('--rhoCoordinates', '-r', type = int, nargs='+', help = 'List of rho coordinates')
+parser.add_argument('--zCoordinates', '-z', type = int, nargs='+', help = 'List of z coordinates')
+parser.add_argument('--distance', '-d', type = int, default = 1, help = 'Distance between coordinates')
+parser.add_argument('--sideLength', '-l', type = int, help = 'Side length of square')
 
 args = parser.parse_args()
+alphaFile = "{}*.root".format(args.alphafile)
+betaFile = "{}*.root".format(args.betafile)
+
+print(alphaFile)
 
 r.gROOT.SetBatch(1) 
 
@@ -38,36 +47,32 @@ else:
 
 rhoCoordinates = []
 zCoordinates = []
-distance = 1
+distance = args.distance
 
-#We discussed some ways to reimplement this on Thursday
-if args.shape == "square"
-        squareLength = int(raw_input("Please enter the side length of your square:"))
-        distance = int(raw_input("Please enter the distance between the coordinates:"))
-        for i in range(-math.floor(squareLength/distance, math.floor(squareLength/distance)
-                for j in range(-math.floor(squareLength/distance, math.floor(squareLength/distance)
-                        rhoCoordinates.extend(i)
-                        zCoordinates.extend(j)
-                       
-if args.shape == "list"
-        rhoCoordinates = list(map(int, raw_input("Please enter the rho coordinates in the form '3 2 3 4 8':").split()))
-        zCoordinates = list(map(int, raw_input("Please enter the z coordinates in the form '4 6 3 8 8':").split()))
-        distance = int(raw_input("Please enter the distance between the coordinates:"))
-                                 
+if args.shape == "square":
+        squareLength = args.sideLength
+        for i in range(-math.floor(squareLength/distance, math.floor(squareLength/distance))):
+                for j in range(-math.floor(squareLength/distance, math.floor(squareLength/distance))):
+                        rhoCoordinates.append(i)
+                        zCoordinates.append(j)                
+
+if args.shape == "list":
+        rhoCoordinates = args.rhoCoordinates
+        zCoordinates = args.zCoordinates
+
+print(rhoCoordinates)                                 
+
 xTicks = []
 yTicks = []
 
-for i in range(0, math.floor(min(rhoCoordinates)-max(rhoCoordinates)/distance))
+for i in range(0, math.floor(min(rhoCoordinates)-max(rhoCoordinates)/distance)):
         xTicks.append(min(rhoCoordinates) + (distance*i))
                                  
-for i in range(0, math.floor(min(zCoordinates)-max(zCoordinates)/distance))
+for i in range(0, math.floor(min(zCoordinates)-max(zCoordinates)/distance)):
         yTicks.append(min(zCoordinates) + (distance*i))
                 
-
-#We can make the ratio an argument with argparse
 # set ratio Alpha/Beta
 ratio = 9
-ratio2 = str(s.significantFigures(ratio,3)).replace(".","-")
 
 ClassifierYoudenArray = []
 ValueYoudenArray = []
@@ -79,6 +84,10 @@ BetaAcceptanceYoudenArray = []
 AlphaRejectionGeneralArray = []
 BetaAcceptanceGeneralArray = []
 
+ClassifierAlphaArray = []
+NhitAlphaArray = []
+ClassifierBetaArray = []
+NhitBetaArray = []
 
 graphs = [ClassifierYoudenArray, ClassifierGeneralArray, ValueYoudenArray, ValueGeneralArray, AlphaRejectionYoudenArray, BetaAcceptanceYoudenArray, AlphaRejectionGeneralArray, BetaAcceptanceGeneralArray, ClassifierAlphaArray, NhitAlphaArray, ClassifierBetaArray, NhitBetaArray]
 titles = ["Youden Classifier Cut Value","General Classifier Cut Value", "Youden Cut Value", "General Cut Value", r"Youden $\alpha$ Rejection", r"Youden $\beta$ Acceptance", r"General $\alpha$ Rejection", r"General $\beta$ Acceptance", r"Classification $\alpha$ Summary",r"$N_{\mathrm{hit}}$ $\alpha$ Summary", r"Classification $\beta$ Summary", r"$N_{\mathrm{hit}}$ $\beta$ Summary"]
@@ -87,17 +96,16 @@ colorbar = ["Classifier Value", "Classifier Value", "Youden Statistic Value", "G
 
 for i in range(0, len(rhoCoordinates)):
 
-        if args.filetype == "ntuple"
-                alphaHistogram = r.NhitHistogram("{}*.root".format(args.alphafile), rhoCoordinates[i], zCoordinates[i], ratio, distance)
-                betaHistogram = r.NhitHistogram("{}*.root".format(args.betafile), rhoCoordinates[i], zCoordinates[i], ratio, distance)
+        if args.filetype == "ntuple":
+                alphaHistogram = r.NhitHistogram(alphaFile, rhoCoordinates[i], zCoordinates[i], ratio, distance)
+                betaHistogram = r.NhitHistogram(betaFile, rhoCoordinates[i], zCoordinates[i], ratio, distance)
                 values = rejectionInfo(alphaHistogram, betaHistogram, ratio)
 
         
-        else if args.filetype == "ratds"
-                                           alphaHist, rhoCoordinate, zCoordinate, distance
-                alphaHistogram = r.NhitHistogram("{}*.root".format(args.alphafile), "{}*.root".format(args.betafile), "partialFitter", "BerkeleyAlphaBeta:partialFitter", "likelihood", alphaHist, rhoCoordinates[i], zCoordinates[i], distance)
-                betaHistogram = r.NhitHistogram("{}*.root".format(args.alphafile), "{}*.root".format(args.betafile), "partialFitter", "BerkeleyAlphaBeta:partialFitter", "likelihood", betaHist, rhoCoordinates[i], zCoordinates[i], distance)
-                values = = rejectionInfo(alphaHistogram, betaHistogram, ratio)
+        elif args.filetype == "ratds":
+                alphaHistogram = r.NhitHistogram(alphaFile, betaFile, "partialFitter", "BerkeleyAlphaBeta:partialFitter", "likelihood", "alphaHist", rhoCoordinates[i], zCoordinates[i], distance)
+                betaHistogram = r.NhitHistogram(alphaFile, betaFile, "partialFitter", "BerkeleyAlphaBeta:partialFitter", "likelihood", "betaHist", rhoCoordinates[i], zCoordinates[i], distance)
+                values = rejectionInfo(alphaHistogram, betaHistogram, ratio)
 
 
         ClassifierYoudenArray.append(values[0])
@@ -117,7 +125,7 @@ for i in range(0, len(rhoCoordinates)):
 for graph in graphs:
         graph.extend([-100, -100, -100])
 
-rhoCoordinates.extend(3, 4, 4)
+rhoCoordinates.extend([3, 4, 4])
 zCoordinates.extend([4, 4, 4])
 
 for i in range(0,12):
@@ -134,10 +142,10 @@ for i in range(0,12):
         p.yticks(yTicks)
         p.colorbar(label=colorbar[i])
         p.show()               
-        p.savefig("partialFillPDFs/SummaryPartialFill{}.pdf".format(graphs2[i]))
+        p.savefig("partialFillPDFsTEST/SummaryPartialFill{}.pdf".format(graphs2[i]))
         p.clf()
 
-def rejectionInfo(alpha_hist, beta_hist, ratio)
+def rejectionInfo(alpha_hist, beta_hist, ratio):
                                            
     alpha_histogram = alpha_hist
     beta_histogram = beta_hist                                       
@@ -163,11 +171,11 @@ def rejectionInfo(alpha_hist, beta_hist, ratio)
         beta_acceptance = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, k)
         beta_rejection = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), k, youdenSelection.GetNbinsY())
 
-        if !(beta_acceptance == 0 && alpha_rejection == 0)
+        if not (beta_acceptance == 0 and alpha_rejection == 0):
             youden_statistic = beta_acceptance/(beta_acceptance+beta_rejection) + alpha_rejection/(alpha_acceptance+alpha_rejection)
             general_statistic = beta_acceptance/sqrt(beta_acceptance+alpha_rejection)
 
-        else
+        else:
             youden_statistic = 0
             general__statistic = 0
 
@@ -176,7 +184,7 @@ def rejectionInfo(alpha_hist, beta_hist, ratio)
         general_histogram.Fill(x, general_statistic)
 
         x += abs(youden_histogram.GetXaxis().GetXmin() - youden_histogram.GetXaxis().GetXmax())/youden_histogram.getNbinsX()
-    }
+    
 
     youdenClassifierBin = youden_histogram.GetMaximumBin()
     youdenClassifierMax = youden_histogram.GetXaxis().GetBinCenter(youdenClassifierBin)
@@ -188,10 +196,10 @@ def rejectionInfo(alpha_hist, beta_hist, ratio)
     allAlphas = alpha_histogram.Integral()
     allBetas = beta_histogram.Integral()
 
-    if allAlphas==0
+    if allAlphas==0:
         allAlphas = 1e-15
    
-    if allBetas == 0
+    if allBetas == 0:
         allBetas = 1e-15
 
     youdenAlphaRejection = alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), youdenClassifierBin, youden_histogram.GetNbinsY()) / allAlphas
