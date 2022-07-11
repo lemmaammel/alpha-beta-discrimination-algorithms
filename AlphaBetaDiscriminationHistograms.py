956270b9267f223a50010aabdecd6a5e496e12ce
skippy <67166525+lemmaammel@users.noreply.github.com>
2022-07-11 11:57:38 -0700

    Update AlphaBetaDiscriminationHistograms.py

--- AlphaBetaDiscriminationHistograms.py
+++ AlphaBetaDiscriminationHistograms.py
@@ -19,6 +19,71 @@ import matplotlib.pyplot as p
 import significantFigures as s
 import findRatio as f
 
+def rejectionInfo(alpha_hist, beta_hist, ratio):
+
+    alpha_histogram = alpha_hist
+    beta_histogram = beta_hist                                       
+
+    #Like in the other file let's report a separate mean for alphas and betas
+    meanNhit = (alpha_histogram.GetMean(1)+beta_histogram.GetMean(1))/(alpha_histogram.Integral()+beta_histogram.Integral())
+
+    # cut selection histograms
+    youden_histogram = TH1D("Youden's J Statistic", "Youden's J Statistic", alpha_histogram.GetNbinsY(),  alpha_histogram.GetMinimum(), alpha_histogram.GetMaximum())
+    general_histogram = TH1D("General Cut Statistic", "General Cut Statistic", alpha_histogram.GetNbinsY(), alpha_histogram.GetMinimum(), alpha_histogram.GetMaximum())
+                                           
+    alpha_rejection = 0
+    alpha_acceptance = 0
+    beta_rejection = 0
+    beta_acceptance = 0
+
+    youden_statistic = 0
+    general_statistic = 0
+
+    for k in range(0, youden_histogram.GetNBinsX()):
+        alpha_acceptance = ratio*alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, k)
+        alpha_rejection = ratio*alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), k, youden_histogram.GetNbinsY())
+        beta_acceptance = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, k)
+        beta_rejection = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), k, youdenSelection.GetNbinsY())
+
+        if not (beta_acceptance == 0 and alpha_rejection == 0):
+            youden_statistic = beta_acceptance/(beta_acceptance+beta_rejection) + alpha_rejection/(alpha_acceptance+alpha_rejection)
+            general_statistic = beta_acceptance/sqrt(beta_acceptance+alpha_rejection)
+
+        else:
+            youden_statistic = 0
+            general_statistic = 0
+
+        #fill for cut selection stats
+        youden_histogram.Fill(x, youden_statistic)
+        general_histogram.Fill(x, general_statistic)
+
+        x += abs(youden_histogram.GetXaxis().GetXmin() - youden_histogram.GetXaxis().GetXmax())/youden_histogram.getNbinsX()
+
+
+    youdenClassifierBin = youden_histogram.GetMaximumBin()
+    youdenClassifierMax = youden_histogram.GetXaxis().GetBinCenter(youdenClassifierBin)
+    generalClassifierBin = general_histogram.GetMaximumBin()
+    generalClassifierMax = general_histogram.GetXaxis().GetBinCenter(generalClassifierBin)
+    youdenNhitMax = youden_histogram.GetMaximum()
+    generalNhitMax = general_histogram.GetMaximum()
+
+    allAlphas = alpha_histogram.Integral()
+    allBetas = beta_histogram.Integral()
+
+    if allAlphas == 0:
+        allAlphas = 1e-15
+   
+    if allBetas == 0:
+        allBetas = 1e-15
+
+    youdenAlphaRejection = alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), youdenClassifierBin, youden_histogram.GetNbinsY()) / allAlphas
+    youdenBetaAcceptance = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, youdenClassifierBin) / allBetas
+    generalAlphaRejection = alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), generalClassifierBin, youden_histogram.GetNbinsY()) / allAlphas
+    generalBetaAcceptance = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, generalClassifierBin) /allBetas
+
+    return [youdenClassifierMax, youdenNhitMax, generalClassifierMax, generalNhitMax, youdenAlphaRejection, youdenBetaAcceptance, generalAlphaRejection, generalBetaAcceptance, meanNhit]
+                 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--filetype', '-f', type = str, default = '', help = 'File type of alpha and beta files ("ntuple" OR "ratds")')
 parser.add_argument('--alphafile', '-a', type = str, default = '', help = 'Alpha file to use (no ".root" extension)')
@@ -140,71 +205,3 @@ for i in range(0,12):
         p.show()               
         p.savefig("partialFillPDFsTEST/SummaryPartialFill{}.pdf".format(graphs2[i]))
         p.clf()
-
-def rejectionInfo(alpha_hist, beta_hist, ratio):
-
-    alpha_histogram = alpha_hist
-    beta_histogram = beta_hist                                       
-
-    #Like in the other file let's report a separate mean for alphas and betas
-    meanNhit = (alpha_histogram.GetMean(1)+beta_histogram.GetMean(1))/(alpha_histogram.Integral()+beta_histogram.Integral())
-
-    # cut selection histograms
-    youden_histogram = TH1D("Youden's J Statistic", "Youden's J Statistic", alpha_histogram.GetNbinsY(),  alpha_histogram.GetMinimum(), alpha_histogram.GetMaximum())
-    general_histogram = TH1D("General Cut Statistic", "General Cut Statistic", alpha_histogram.GetNbinsY(), alpha_histogram.GetMinimum(), alpha_histogram.GetMaximum())
-                                           
-    alpha_rejection = 0
-    alpha_acceptance = 0
-    beta_rejection = 0
-    beta_acceptance = 0
-
-    youden_statistic = 0
-    general_statistic = 0
-
-    for k in range(0, youden_histogram.GetNBinsX()):
-        alpha_acceptance = ratio*alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, k)
-        alpha_rejection = ratio*alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), k, youden_histogram.GetNbinsY())
-        beta_acceptance = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, k)
-        beta_rejection = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), k, youdenSelection.GetNbinsY())
-
-        if not (beta_acceptance == 0 and alpha_rejection == 0):
-            youden_statistic = beta_acceptance/(beta_acceptance+beta_rejection) + alpha_rejection/(alpha_acceptance+alpha_rejection)
-            general_statistic = beta_acceptance/sqrt(beta_acceptance+alpha_rejection)
-
-        else:
-            youden_statistic = 0
-            general_statistic = 0
-
-        #fill for cut selection stats
-        youden_histogram.Fill(x, youden_statistic)
-        general_histogram.Fill(x, general_statistic)
-
-        x += abs(youden_histogram.GetXaxis().GetXmin() - youden_histogram.GetXaxis().GetXmax())/youden_histogram.getNbinsX()
-
-
-    youdenClassifierBin = youden_histogram.GetMaximumBin()
-    youdenClassifierMax = youden_histogram.GetXaxis().GetBinCenter(youdenClassifierBin)
-    generalClassifierBin = general_histogram.GetMaximumBin()
-    generalClassifierMax = general_histogram.GetXaxis().GetBinCenter(generalClassifierBin)
-    youdenNhitMax = youden_histogram.GetMaximum()
-    generalNhitMax = general_histogram.GetMaximum()
-
-    allAlphas = alpha_histogram.Integral()
-    allBetas = beta_histogram.Integral()
-
-    if allAlphas == 0:
-        allAlphas = 1e-15
-   
-    if allBetas == 0:
-        allBetas = 1e-15
-
-    youdenAlphaRejection = alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), youdenClassifierBin, youden_histogram.GetNbinsY()) / allAlphas
-    youdenBetaAcceptance = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, youdenClassifierBin) / allBetas
-    generalAlphaRejection = alpha_histogram.Integral(1, youden_histogram.GetNbinsX(), generalClassifierBin, youden_histogram.GetNbinsY()) / allAlphas
-    generalBetaAcceptance = beta_histogram.Integral(1, youden_histogram.GetNbinsX(), 1, generalClassifierBin) /allBetas
-
-    return [youdenClassifierMax, youdenNhitMax, generalClassifierMax, generalNhitMax, youdenAlphaRejection, youdenBetaAcceptance, generalAlphaRejection, generalBetaAcceptance, meanNhit]
-                                           
-                                           
-                                           
-                                           
